02965b4c26aa1323b13665f7c0d10a7e8c5907a4
burakovas <burakovas@gmail.com>
2019-02-20 21:27:57 +0300

    Rest added

--- common/config/main.php
+++ common/config/main.php
@@ -6,10 +6,7 @@ return [
     ],
     'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
     'components' => [
-        'bot' => [
-            'class' => '\SonkoDmitry\Yii\TelegramBot\Component',
-            'apiToken' => '645770281:AAGs2XfMsFr09ZTPquauj6VV_hQ_aDHOmOs'
-        ],
+
         'cache' => [
             'class' => 'yii\caching\FileCache',
         ],
---
+++ frontend/controllers/RtasksController.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: burakovas
+ * Date: 2019-02-20
+ * Time: 19:50
+ */
+
+namespace frontend\controllers;
+
+use common\models\tables\Tasks;
+use yii\rest\ActiveController;
+
+class RtaskController extends ActiveController
+{
+    public $modelClass = Tasks::class;
+
+}
